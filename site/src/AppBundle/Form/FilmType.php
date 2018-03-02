c64210002612a998a69b677aeef6b3e0af30be22
Unknown <vibitashruti@gmail.com>
2018-03-02 16:56:39 +0100

    remove extra use

--- site/src/AppBundle/Form/FilmType.php
+++ site/src/AppBundle/Form/FilmType.php
@@ -9,12 +9,6 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 /**/
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 
 class FilmType extends AbstractType
